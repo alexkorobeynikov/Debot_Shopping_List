0f23de127d14aba6368cbe85676a14b24c18b34b
alexkorobeynikov <alexkorobeynikov@gmail.com>
2021-11-09 19:32:55 +0400

    2nd

--- DebotInitList.sol
+++ DebotInitList.sol
@@ -7,6 +7,111 @@ import "PurchaseListInterfaces.sol";
 abstract contract DebotInitList is  Debot, Upgradable {
 
 
+   TvmCell m_code; //  contract code
+    address m_address;  // TD contract address
+    SummaryPurchase m_stat;        // Statistics
+    uint32 m_PurchaseId;    // ID
+    uint256 m_masterPubKey; // User pubkey
+    address m_msigAddress;  // User wallet address
+
+    uint32 INITIAL_BALANCE =  100000000;  // balance
+
+    function setTodoCode(TvmCell code) public {
+        require(msg.pubkey() == tvm.pubkey(), 101);
+        tvm.accept();
+        m_code = code;
+    }
+
+    function onError(uint32 sdkError, uint32 exitCode) public {
+        Terminal.print(0, format("Operation failed. sdkError {}, exitCode {}", sdkError, exitCode));
+        _menu();
+    }
+
+
+     function onSuccess() public view {
+        _getStat(tvm.functionId(setStat));
+    }
+
+    function start() public override {
+        Terminal.input(tvm.functionId(savePublicKey),"Please enter your public key",false);
+    }
+
+     function getDebotInfo() public functionID(0xDEB) override view returns(
+        string name, string version, string publisher, string key, string author,
+        address support, string hello, string language, string dabi, bytes icon
+    ) {
+        name = "List of Purchase DeBot";
+        version = "0.1";
+        publisher = "Alexander Korobeynikov";
+        key = "purchase list";
+        author = "Alexander Korobeynikov";
+        support = address.makeAddrStd(0, 0x66e01d6df5a8d7677d9ab2daf7f258f1e2a7fe73da5320300395f99e01dc3b5f);
+        hello = "Hi, I'm List of Purchase";
+        language = "en";
+        dabi = m_debotAbi.get();
+        icon = m_icon;
+    }
+
+    function savePublicKey(string value) public {
+        (uint res, bool status) = stoi("0x"+value);
+        if (status) {
+            m_masterPubKey = res;
+
+            Terminal.print(0, "Checking if you already have a Purchase list ...");
+            TvmCell deployState = tvm.insertPubkey(m_code, m_masterPubKey);
+            m_address = address.makeAddrStd(0, tvm.hash(deployState));
+            Terminal.print(0, format( "Info: your Purchase list contract address is {}", m_address));
+            Sdk.getAccountType(tvm.functionId(checkStatus), m_address);
+
+        } else {
+            Terminal.input(tvm.functionId(savePublicKey),"Wrong public key. Try again!\nPlease enter your public key",false);
+        }
+    }
+
+
+    function checkStatus(int8 acc_type) public {
+        if (acc_type == 1) { // acc is active and  contract is already deployed
+            _getStat(tvm.functionId(setStat));
+
+        } else if (acc_type == -1)  { // acc is inactive
+            Terminal.print(0, "You don't have a purchase list yet, so a new contract with an initial balance of 0.2 tokens will be deployed");
+            AddressInput.get(tvm.functionId(creditAccount),"Select a wallet for payment. We will ask you to sign two transactions");
+
+        } else  if (acc_type == 0) { // acc is uninitialized
+            Terminal.print(0, format(
+                "Deploying new contract. If an error occurs, check if your Purchase List contract has enough tokens on its balance"
+            ));
+            deploy();
+
+        } else if (acc_type == 2) {  // acc is frozen
+            Terminal.print(0, format("Can not continue: account {} is frozen", m_address));
+        }
+    }
+
+
+    function creditAccount(address value) public {
+        m_msigAddress = value;
+        optional(uint256) pubkey = 0;
+        TvmCell empty;
+        IMsig(m_msigAddress).sendTransaction{
+            abiVer: 2,
+            extMsg: true,
+            sign: true,
+            pubkey: pubkey,
+            time: uint64(now),
+            expire: 0,
+            callbackId: tvm.functionId(waitBeforeDeploy),
+            onErrorId: tvm.functionId(onErrorRepeatCredit)  // Just repeat if something went wrong
+        }(m_address, INITIAL_BALANCE, false, 3, empty);
+    }
+
+    function onErrorRepeatCredit(uint32 sdkError, uint32 exitCode) public {
+        // TODO: check errors if needed.
+        sdkError;
+        exitCode;
+        creditAccount(m_msigAddress);
+    }
+
 
 
 
